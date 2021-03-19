598c37515770b67c3a2929d91eb543a946acbf9d
Christian Øren <Christian.oren@hotmail.no>
2021-03-19 09:31:49 +0100

    wip.. added main file and reise component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import React, { useState } from 'react';
+import { Reise } from './components/Reise';
+
 function App() {
-  return (
-    <div className="App">
-      ...
-    </div>
-  );
+    const [state, setState] = useState({
+		arbeidsreiser: []
+    });
+
+	const Arbeidsreise = 'arbeidsreise';
+	const Besoeksreise = 'besoeksreise';
+
+	const handleOnChangeArbeidsreise = (arbeidsreise, type) => {
+		if (type === Arbeidsreise) {
+			var arbeidsreiserJoined = state.arbeidsreiser;
+			arbeidsreiserJoined.push(arbeidsreise)
+			setState({arbeidsreiser: arbeidsreiserJoined});
+			console.log('Updated App state');
+		}
+	}
+
+    return (
+        <main className="App">
+            <div
+                style={{
+                    width: '600px',
+                    border: '1px solid gray',
+                    padding: '10px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    flexDirection: 'column',
+                }}
+            >
+                <h1 style={{ color: '#1362ae' }}>
+                    Legg inn dine arbeidsreiser
+                </h1>
+
+				<Reise id={`arbeidsreiseId`} onChange={handleOnChangeArbeidsreise} type='arbeidsreise' />
+			</div>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/Reise.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+export const Reise = ({ id, onChange, type }) => {
+    const [state, setState] = useState({
+        km: 0,
+        antall: 0,
+    });
+
+    const [error, setError] = useState({
+        message: '',
+    });
+
+    const resetState = {
+        km: 0,
+        antall: 0,
+    };
+
+    const resetErrors = {
+        message: '',
+    };
+
+    const handleOnChange = (event) => {
+        setState({ ...state, ...{ [event.target.name]: event.target.value } });
+    };
+
+    const validate = () => {
+        if (state.antall <= 0) {
+            setError({ message: 'Antall kan ikke være mindre enn eller lik 0.' });
+        }
+
+        if (state.km <= 0) {
+            setError({ message: 'Kilometer kan ikke være mindre enn eller lik 0.' });
+        }
+
+        if (state.antall > 0 && state.km > 0) {
+            setError(resetErrors);
+            return true;
+        }
+    };
+
+    const handleOnClick = () => {
+        const isValid = validate();
+        if (!isValid) {
+            return;
+        }
+
+        onChange(state, type);
+        setState(resetState);
+    };
+
+    return (
+        <>
+            <div style={{ minWidth: '100%', height: '30px' }}>{error.message}</div>
+            <div>
+                <label htmlFor={id} style={{ width: '100%' }}>
+                    {' '}
+                    Kilometer:{' '}
+                </label>
+                <input value={state.km} type="number" id={id} name="km" onChange={handleOnChange} />
+            </div>
+
+            <div>
+                <label htmlFor={id}> Antall: </label>
+                <input value={state.antall} type="number" id={id} name="antall" onChange={handleOnChange} />
+            </div>
+
+            <button onClick={handleOnClick}>+</button>
+        </>
+    );
+};
