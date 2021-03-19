4c0e9984478611f24a4e67e3de63ffa34597a027
Christian Øren <Christian.oren@hotmail.no>
2021-03-19 13:28:14 +0100

    added css stylesheet, added error handling to fetch request, removing inline styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Reise } from './components/Reise';
 import { Utgifter } from './components/Utgifter';
+import './styling/index.css';
 
 function App() {
     const [state, setState] = useState({
@@ -9,11 +10,11 @@ function App() {
         utgifterBomFergeEtc: 0,
     });
 
-	const resetState = {
-		arbeidsreiser: [],
+    const resetState = {
+        arbeidsreiser: [],
         besoeksreiser: [],
         utgifterBomFergeEtc: 0,
-	};
+    };
 
     const [reisefradrag, setReisefradrag] = useState();
 
@@ -42,54 +43,67 @@ function App() {
         }
     };
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(state),
         };
-        const response = await fetch('https://localhost:5001/api/ReiseFradrag', requestOptions);
-        const data = await response.json();
-        setReisefradrag(data.reisefradrag);
-		setState(resetState);
+        const response = await fetch('https://localhost:5001/api/ReiseFradrag', requestOptions)
+            .then(async (response) => {
+                const data = await response.json();
+
+                if (!response.ok) {
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+
+                setReisefradrag(data.reisefradrag);
+                setState(resetState);
+            })
+            .catch((error) => {
+                // handle error
+            });
     };
 
     return (
         <main className="App">
-            <h1>Reisefradragstjenesten</h1>
-            <div
-                style={{
-                    width: '600px',
-                    border: '1px solid gray',
-                    padding: '10px',
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    flexDirection: 'column',
-                }}
-            >
-                <Reise
-                    id={`${Arbeidsreise}Id`}
-                    onChange={handleStateChange}
-                    type={Arbeidsreise}
-                    tittel="Registrer dine arbeidsreiser"
-                />
+            <div className="wrapper">
+                <form onSubmit={handleSubmit}>
+                    <Reise
+                        id={`${Arbeidsreise}Id`}
+                        onChange={handleStateChange}
+                        type={Arbeidsreise}
+                        tittel="Arbeidsreiser"
+                    />
 
-                <Reise
-                    id={`${Besoeksreise}Id`}
-                    onChange={handleStateChange}
-                    type={Besoeksreise}
-                    tittel="Registrer dine besøksreiser"
-                />
+                    <Reise
+                        id={`${Besoeksreise}Id`}
+                        onChange={handleStateChange}
+                        type={Besoeksreise}
+                        tittel="Besøksreiser"
+                    />
 
-                <Utgifter
-                    id={`${UtgifterTilBomFergeEtc}Id`}
-                    onChange={handleStateChange}
-                    type={UtgifterTilBomFergeEtc}
-                    tittel="Registrer dine bom og ferge utgifter"
-                />
+                    <Utgifter
+                        id={`${UtgifterTilBomFergeEtc}Id`}
+                        onChange={handleStateChange}
+                        type={UtgifterTilBomFergeEtc}
+                        tittel="Bom og ferge utgifter"
+                    />
+                    <button type="submit" style={{ display: 'flex', margin: '20px auto 20px auto' }}>
+                        Send inn
+                    </button>
+                </form>
             </div>
-            <button onClick={handleSubmit}>Send inn</button>
-            {reisefradrag}
+            <div className="wrapper reisefradrag">
+                {reisefradrag && (
+                    <div>
+                        <h3>Reisefradrag</h3>
+                        <p>{reisefradrag}</p>
+                    </div>
+                )}
+            </div>
         </main>
     );
 }
